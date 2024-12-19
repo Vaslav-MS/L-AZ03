e289a7f0b9af1ea63b675a95b978eff345a5aaad
Vaslav <2key@rambler.ru>
2024-12-19 15:20:46 +0500

    1 code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
+x = np.linspace(-100, 100, 100000)
+y = x ** 64
 
+plt.plot(x,y)
+plt.title('График: y = x ** 2')
+plt.grid(True)
+
+plt.show()
