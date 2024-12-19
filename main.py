665c602075f55ee796a6898334f99c7b84568861
Vaslav <2key@rambler.ru>
2024-12-19 15:32:38 +0500

    diagramm code

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Параметры нормального распределения
+# 1
+
 mean = 0  # Среднее значение
 std_dev = 1  # Стандартное отклонение
 num_samples = 10000  # Количество образцов
-
 data = np.random.normal(mean, std_dev, num_samples)
 
-#plt.figure(figsize=(8, 6))
 plt.hist(data, bins=50, edgecolor='black', alpha=0.7)
 plt.title('Гистограмма')
 plt.xlabel('Значение')
 plt.ylabel('Частота')
 plt.grid(True)
+plt.show()
+
+# 2
+
+points = 50  # Количество точек
+x = np.random.rand(points)  # X
+y = np.random.rand(points)  # Y
+
+plt.scatter(x, y, alpha=0.7, edgecolor='black')
+plt.title('Диаграмма рассеяния')
 plt.show()
